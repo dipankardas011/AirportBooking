c93f8fdb8b6f959aee59cadbe5534d448f627d57
Dipankar Das <dipsonu10@hotmail.com>
2022-05-13 13:03:25 +0530

    Correction in script

--- Runner.sh
+++ Runner.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-Booking.sh
+./Booking.sh
 
-ImageBuilder.sh
+./ImageBuilder.sh
 
 
 echo $(tput setaf 4)$(tput bold)DEPLOYED$(tput init) booked and image generated
 
 echo 'Enter Y/N for deletion of resources'
 read choice
-if [ $choice -eq 'Y' ]; then
+if [ $choice == 'Y' ]; then
   docker volume rm airplane
   docker rmi airplane-img
   docker rmi airplane-jdk
 
-  echo $(tput setaf 4)$(tput bold)CLEANED$(tput init) " Done🧹"
-done
+  echo $(tput setaf 2)$(tput bold)CLEANED$(tput init) " Done🧹"
+fi
 
